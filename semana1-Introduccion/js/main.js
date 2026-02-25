a7eb52af364d95c906fd2133a017bf13a638822a
jesusDev96 <chechujor@hotmail.com>
2026-02-24 22:36:10 -0300

    variables, prompt y parseInt, semana 1

---
+++ semana1-Introduccion/js/main.js
@@ -0,0 +1,21 @@
+// let materia = "javascript"
+// let comision = 89630
+// let flex = true
+
+// const academia = "coderhouse"
+
+// materia = "desarrollo web"
+
+// materia = prompt("Ingrese la materia")
+// comision = prompt("Ingrese el numero de comision")
+
+// console.log("Materia: " + materia)
+// console.log("Comision N°"+comision)
+// console.log("Materia: " + materia + " Comision N°" + comision)
+// alert("Materia: " + materia + " Comision N°" + comision)
+
+
+let numeroA = parseInt(prompt("Ingrese el primer numero")) 
+let numeroB = parseInt(prompt("Ingrese el segundo numero"))
+let resultado = numeroA+numeroB
+console.log(resultado)
